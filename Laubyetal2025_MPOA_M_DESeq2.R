daaa91c07b0639c26f68ea97f8d38b779a9fe2bc
SLauby <116187970+SLauby@users.noreply.github.com>
2025-10-13 08:47:42 -0500

    Update Laubyetal2025_MPOA_M_DESeq2.R

--- Laubyetal2025_MPOA_M_DESeq2.R
+++ Laubyetal2025_MPOA_M_DESeq2.R
@@ -7,10 +7,10 @@ library(readxl)
 #####DEG Analysis####
 #Male mPFC
 #Import tagseq counts and study variable files to R
-MPOA_tagseq_M <- read_excel("./data/MPOA_tagseq_counts_M.xlsx")
+MPOA_tagseq_M <- read_excel("./data/MPOA_tagseq_counts_M_outliersremoved.xlsx")
 View(MPOA_tagseq_M)
 
-MPOA_design_M <- read_excel("./data/MPOA_tagseq_variables_M.xlsx")
+MPOA_design_M <- read_excel("./data/MPOA_tagseq_variables_M_outliersremoved.xlsx")
 View(MPOA_design_M)
 
 #Format file for DESeq2 analysis
